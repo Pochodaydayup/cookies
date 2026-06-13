26b81e6b3858d4ebd46b143796b4944e8599ac2a
pocho.zhou <pocho.zhou@zoomex.com>
2026-06-13 23:45:30 +0800

    feat: init Vite + React + TypeScript project with global styles

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+function App() {
+  return (
+    <div style={{ padding: 24, textAlign: 'center', color: 'var(--color-neon-cyan)' }}>
+      重庆本地美食地图
+    </div>
+  )
+}
+
+export default App
